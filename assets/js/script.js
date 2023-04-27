cf33f6eb854a2f50a0f0ebf92624071af644e0cf
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-04-26 21:43:48 -0700

    stored values in array, mins time function, replay function scoreboard clickthrough but unable to display properly , Basic CSS stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
 //sets default time for timer
+var mainEl = document.querySelector("main"); //main area
+var checkEl = document.querySelector(".check"); //check score area
 var timeEl = document.querySelector(".time"); //selects area where timer is.
 var start = document.querySelector(".start"); //selects button tag by class
+var checkScore = document.createElement("div");
 var questionAreaEl = document.querySelector(".questionArea"); //Selects question area
 var q = document.createElement("ul"); //Ul initiated for type of El
 var h1El = document.createElement("h2");
 var restart = document.createElement("button");
-var qCounter = 0; //needed a counter to cycle to the questions within the array
-var right = 0;
-var wrong = 0;
+var saveScore = document.createElement("button");
 
+var qCounter = 0; //needed a counter to cycle to the questions within the array
+var secondsLeft = 30;
+var playerStats = [];
+var player = {
+  right: 0,
+  wrong: 0,
+  score: 0,
+};
 //arracy of objects to hold questions and answers
 var questions = [
   {
-    question: "Favorite food?",
+    question: "Best food?",
     answers: ["Sushi", "BBQ", "Pizza", "Kdogs"],
     correct: 3,
   },
@@ -31,39 +40,48 @@ var questions = [
     ],
     correct: 2,
   },
+  {
+    question: "Best game",
+    answers: ["Zelda", "StarFox", "Mario", "Sonic"],
+    correct: 1,
+  },
+  {
+    question: "Best Board Game?",
+    answers: ["Here to Slay", "Secret Hitler", "Space Base", "Moon Rakers"],
+    correct: 1,
+  },
+  {
+    question: "Best Rage game",
+    answers: ["League", "Overwatch", "Valorant", "Apex"],
+    correct: 0,
+  },
+  {
+    question: "which Symbol means OR?",
+    answers: ["&&", "||", "<>", "()"],
+    correct: 3,
+  },
 ];
+
+// Start timer when start button is clicked
+start.addEventListener("click", function () {
+  setTime();
+  questionArea();
+  start.remove(); //Remove  start button
+});
 
 //timer function
 function setTime() {
-  var secondsLeft = 5;
   var timeInterval = setInterval(function () {
     timeEl.textContent = "Timer: " + secondsLeft + " seconds left"; //replace context so we can see timer as it decrements
     secondsLeft--; //decrements timer
 
     //when the timer hits 0 stop decrements
-    if (secondsLeft === -1 || questions.length === qCounter) {
+    if (secondsLeft <= -1 || questions.length === qCounter) {
       clearInterval(timeInterval);
-      secondsLeft = 5; //reset timer
+      q.remove();
+      quizComplete();
     }
   }, 1000);
-}
-
-//function used to create an elements to populate questions and answers
-//then populates questions and answers
-function quizComplete() {
-  q.remove();
-  score = (right / 3) * 100;
-  questionAreaEl.appendChild(h1El);
-  h1El.textContent = "Congratulations your Score is " + score;
-  questionAreaEl.appendChild(restart);
-  restart.textContent = "Play again!";
-  restart.addEventListener("click", function () {
-    location.reload();
-  });
-  // restart.addEventListener("click", function{
-  //   questionAreaEl.remove()
-  //   set
-  // });
 }
 
 function questionArea() {
@@ -75,20 +93,19 @@ function questionArea() {
 
     q.appendChild(ans); //creates Els for each answer
     ans.textContent = questions[qCounter].answers[i]; // populates with answer options
+    //adds click event listener to each answer
     ans.addEventListener("click", function () {
-      //adds click event listener to each answer
-      //check answer function;
       if (questions[qCounter].correct === i) {
-        right = right + 1;
+        player.right += 1;
       } else {
-        wrong += 1;
+        player.wrong += 1;
+        secondsLeft = secondsLeft - 3;
       }
       qCounter++; //Quesetion marker up
       q.remove(); //remove previous question
       //this will verify if the test is complete
       if (questions.length === qCounter) {
         quizComplete();
-        return score;
       } else {
         questionArea();
       } //rebuild with new question
@@ -96,10 +113,55 @@ function questionArea() {
   }
 }
 
-// Start timer when start button is clicked
-start.addEventListener("click", function () {
-  setTime();
-  questionArea();
-  //Remove  start button
-  start.remove();
+//function used to create an elements to populate questions and answers
+//then populates questions and answers
+function quizComplete() {
+  player.score = (player.right / questions.length) * 100;
+
+  questionAreaEl.appendChild(h1El);
+  h1El.textContent = "Congratulations your Score is " + player.score;
+  questionAreaEl.appendChild(restart);
+  restart.textContent = "Play again!";
+  questionAreaEl.appendChild(saveScore);
+  saveScore.textContent = "Save Score";
+
+  restart.addEventListener("click", function () {
+    location.reload();
+  });
+
+  saveScore.addEventListener("click", function () {
+    addPlayer(player.score);
+    location.reload();
+  });
+}
+
+//set and get function
+function addPlayer(playerScore) {
+  var player = []; // create an array to store objects
+  letter = prompt("Please enter initials");
+  //create an object to store locally
+  var playerStats = {
+    initials: letter,
+    score: playerScore,
+  };
+
+  player.unshift(playerStats); //push to top of list (it's entering backwards for some reason)
+  player = player.concat(JSON.parse(localStorage.getItem("player") || "[]")); // get item out to amend new data
+
+  localStorage.setItem("player", JSON.stringify(player)); //amend new data
+}
+
+checkEl.addEventListener("click", function () {
+  mainEl.appendChild(checkScore); //add section for scores
+  checkScore.textContent =
+    "sorry not here at the moment please check console application"; // have it say score board
+
+  for (
+    i = 0;
+    i < JSON.parse(localStorage.getItem("player")).length;
+    i++ //create a loop to cycle through objects in array
+  )
+    var scoreboard = JSON.parse(localStorage.getItem("player")); //parse
+  scoreString = JSON.stringify(scoreboard); //stringify
+  console.log(scoreString); // i cant get the onject to come out properly
 });
